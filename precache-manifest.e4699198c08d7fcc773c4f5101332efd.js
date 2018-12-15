37f84e2605ef9010aa0b2466f5e1788c1cf2127e
ridait <rejra2012@gmail.com>
2018-12-16 00:02:32 +0100

    Updates

---
+++ precache-manifest.e4699198c08d7fcc773c4f5101332efd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "90ba072e34f32270a5a3",
+    "url": "/counter-react/static/js/runtime~main.90ba072e.js"
+  },
+  {
+    "revision": "0ccd696afc00e3d15d31",
+    "url": "/counter-react/static/js/main.0ccd696a.chunk.js"
+  },
+  {
+    "revision": "77939b50bf542e8b5928",
+    "url": "/counter-react/static/js/1.77939b50.chunk.js"
+  },
+  {
+    "revision": "77939b50bf542e8b5928",
+    "url": "/counter-react/static/css/1.514124fd.chunk.css"
+  },
+  {
+    "revision": "5e44ea4db9a9e7f0426464ed036a4875",
+    "url": "/counter-react/index.html"
+  }
+];
